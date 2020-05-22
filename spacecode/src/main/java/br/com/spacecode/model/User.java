69989d4590fbae4bf151b10e199e4261dad0d300
Gabriela Guedes <zooiv3rde@gmail.com>
2020-05-22 11:48:49 -0300

    test mongodb

---
+++ spacecode/src/main/java/br/com/spacecode/model/User.java
@@ -0,0 +1,68 @@
+package br.com.spacecode.model;
+
+import io.quarkus.mongodb.panache.MongoEntity;
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+
+@MongoEntity(collection = "user")
+public class User extends PanacheMongoEntity {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String subject;
+    private String message;
+
+    public User(String name, String email, String phone, String subject, String message) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    public User() {}
+
+    public static User findByName(String name) {
+        return find("name", name).firstResult();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
