ded821dab2040ec75619423ed3f58fffb9fc0893
Gabriela Guedes <zooiv3rde@gmail.com>
2020-06-08 15:15:56 -0300

    fix

--- spacecode/src/test/java/br/com/spacecode/controller/ServiceControllerTest.java
+++ spacecode/src/test/java/br/com/spacecode/controller/ServiceControllerTest.java
@@ -3,14 +3,19 @@ package br.com.spacecode.controller;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
+import javax.inject.Inject;
+
 import org.junit.jupiter.api.Test;
 
+import br.com.spacecode.repository.ServiceRepository;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
 @QuarkusTest
 class ServiceControllerTest {
-
+	
+	@Inject
+	ServiceRepository service;
 
 	@Test
 	public void testFindAll() {
@@ -21,27 +26,5 @@ class ServiceControllerTest {
 			.statusCode(200)
 			.body("size()", equalTo(1))
 			.contentType(ContentType.JSON);
-	}
-
-//	  @Test
-//		public void createSuccessTest() {
-//		  User user = new User();
-//		  user.setEmail("email");
-//		  user.setMessage("message");
-//		  user.setName("name");
-//		  user.setPhone("phone");
-//		  user.setSubject("subject");
-//		  
-//		  String headerLocation = given()
-//					.contentType(ContentType.JSON)
-//					.body(user)
-//				.when()
-//					.post("/products")
-//				.then()
-//					.statusCode(HttpServletResponse.SC_CREATED)
-//					.header(HttpHeaderNames.LOCATION, containsString("products/"))
-//					.extract()
-//					.header(HttpHeaderNames.LOCATION);
-//	  }
-
+	}	
 }
